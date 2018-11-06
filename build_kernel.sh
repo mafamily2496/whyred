934ca16a844a272bb47016f7a19425fd753bda2b
mafamily2496 <mafamily2496@126.com>
2018-11-06 21:50:42 +0800

    change build shell

--- build_kernel.sh
+++ build_kernel.sh
@@ -2,13 +2,16 @@ export CCACHE=ccache
 export DEFCONFIG=whyred-perf_defconfig
 export ARCH=arm64
 export CROSS_COMPILE=/run/media/majiaming/hdd1/gcc-linaro-7.3.1-2018.05-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-
-export OUTDIR=/run/media/majiaming/hdd1/whyred
+export CLANG_PATH=/run/media/majiaming/hdd1/DragonTC-8.0-master/bin
+export PATH=${CLANG_PATH}:${PATH}
+export CLANG_TRIPLE=aarch64-linux-gnu-
+export OUTDIR=/run/media/majiaming/hdd1/out/whyred
 #make mrproper O=$OUTDIR
 make -j4 $DEFCONFIG O=$OUTDIR
 make -j4 O=$OUTDIR
-for i in `find -name *.ko`; do cp $i ../anykernel/modules/; done
+for i in `find -name *.ko`; do cp $i ~/anykernel/modules/; done
 #./dtbToolCM -s 2048 -d "qcom,msm-id = <" -2 -o arch/arm/boot/dt.img -p /usr/bin/ arch/arm/boot/
-cp $OUTDIR/arch/arm64/boot/Image.gz-dtb ../anykernel/Image.gz-dtb
+cp $OUTDIR/arch/arm64/boot/Image.gz-dtb ~/anykernel/Image.gz-dtb
 #find $objdir -name '*.ko' -exec cp -av {} ../anykernel/modules/ \;
 #cd ~/anykernel/AnyKernel2
 #DATE=$(date +"%m-%d-%y")
